4274bbf920354871f80732de4bec8b1e4f45ff0b
Benbentwo <ben.smith.developer@gmail.com>
2024-01-23 23:09:01 -0800

    Bugfix crash

--- Source/ALS/Private/AlsCharacter.cpp
+++ Source/ALS/Private/AlsCharacter.cpp
@@ -385,7 +385,7 @@ void AAlsCharacter::RefreshMeshProperties() const
 		}
 	}
 
-	if (!bMeshIsTicking)
+	if (!bMeshIsTicking && AnimationInstance.IsValid())
 	{
 		AnimationInstance->MarkPendingUpdate();
 	}
